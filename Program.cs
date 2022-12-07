f039d5c485bcde4cd54585934eef033e4abe95de
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2022-12-07 13:29:57 -0300

    Create the arguments that will hold the prices

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿using parking_lot_system.Models;
 
+decimal priceInitial = 0m;
+decimal pricePerHour = 0m;
+
+Console.WriteLine("Welcome to the parking system!\n" +
+                  "Enter the starting price:");
+priceInitial = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Now enter the price per hour:");
+pricePerHour = Convert.ToDecimal(Console.ReadLine());
