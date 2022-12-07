32309a9ef8a7b2dd2d515308a7ff54aba11fe438
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2022-12-07 10:51:13 -0300

    Create the necessary attributes for the class

--- Models/Parking.cs
+++ Models/Parking.cs
@@ -7,6 +7,13 @@ namespace parking_lot_system.Models
 {
     public class Parking
     {
-        
-    }
+        private decimal priceInitial = 0;
+        private decimal pricePerHour = 0;
+        private List<string> vehicles = new List<string>();
+
+        public Parking(decimal priceInitial, decimal pricePerHour)
+        {
+            this.priceInitial = priceInitial;
+            this.pricePerHour = pricePerHour;
+        }
 }
